64b3c148f5b02d2117715e88adeeedad34cd04c1
Iris Olvera Developer <iris.oeti98@gmail.com>
2022-02-21 12:53:00 -0600

    eureka

--- cita-service/src/main/java/com/inmobiliaria/citaservice/CitaServiceApplication.java
+++ cita-service/src/main/java/com/inmobiliaria/citaservice/CitaServiceApplication.java
@@ -2,8 +2,10 @@ package com.inmobiliaria.citaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CitaServiceApplication {
 
 	public static void main(String[] args) {
