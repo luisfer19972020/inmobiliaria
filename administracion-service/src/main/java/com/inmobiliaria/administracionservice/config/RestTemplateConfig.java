6975674313d58c46e6229d1e6cbb6648d10aaa70
luisfer19972020 <lcorrea.softsol@gmail.com>
2022-02-21 10:18:17 -0600

    eureka

--- administracion-service/src/main/java/com/inmobiliaria/administracionservice/config/RestTemplateConfig.java
+++ administracion-service/src/main/java/com/inmobiliaria/administracionservice/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.inmobiliaria.administracionservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
     @Bean
+    @LoadBalanced//Gestiona las instancias del servicio en eureka esto para que haya multiples servidores apoyando un servicio
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
--- administracion-service/src/main/java/com/inmobiliaria/administracionservice/models/feingclients/PropiedadFeingClient.java
+++ administracion-service/src/main/java/com/inmobiliaria/administracionservice/models/feingclients/PropiedadFeingClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-@FeignClient(name = "propiedad-service", url = "http://localhost:8003")
+@FeignClient(name = "propiedad-service")
 public interface PropiedadFeingClient {
     @PostMapping("/propiedad")
     public Propiedad save(@RequestBody Propiedad propiedad);
